e03919b0e878ace576d61a2eee12c12065f877d7
arvindpndit <arvindpandit9808@gmail.com>
2023-08-11 09:34:51 +0530

    message preview added

--- src/components/MessagePreview.jsx
+++ src/components/MessagePreview.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-const MessagePreview = () => {
+const MessagePreview = ({ name, text, dp }) => {
   return (
-    <div className="flex  items-center  gap-2 md:gap-4 py-3 w-full md:w-4/5 mx-auto px-2 md:px-6 lg:px-8 ">
+    <div className="flex  items-center  gap-2 md:gap-4 py-3 w-full md:w-4/5 mx-auto px-2 md:px-6 lg:px-8 hover:bg-slate-50 rounded-md">
       <div className="w-12 h-12 md:w-16 md:h-16">
         <img
-          src="../../public/assets/images/avatar.png"
+          src={dp}
           alt="Profile"
           className="rounded-full object-cover w-full h-full"
         />
       </div>
       <div className="flex-1 ">
-        <div className="font-semibold text-base">arvind 🌻</div>
-        <div className="text-xs md:text-sm text-gray-600">
-          Hi, I'm happy to help today. I build applications which eventually
-          makes the lives of humans easy.
-        </div>
+        <div className="font-semibold text-base">{name}</div>
+        <div className="text-xs md:text-sm text-gray-600">{text}</div>
       </div>
       <div className="text-xs md:text-sm text-gray-400">Wed</div>
     </div>
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,14 +2,63 @@ import React from "react";
 import CommonHeader from "./CommonHeader";
 import MessagePreview from "./MessagePreview";
 
+const messages = [
+  {
+    name: "arvind",
+    message: `Hi, I'm Arvind. I'm passionate about crafting software solutions that make the world a better place.`,
+    profilePic: ".../../public/assets/images/dp/1.jpg",
+  },
+  {
+    name: "shreya",
+    message: `Hey, this is Shreya. I enjoy creating applications that simplify people's lives and bring joy.`,
+    profilePic: ".../../public/assets/images/dp/2.jpg",
+  },
+  {
+    name: "prashant",
+    message: `Hello, I'm Prashant. I love building applications that have a positive impact on society.`,
+    profilePic: ".../../public/assets/images/dp/3.jpg",
+  },
+  {
+    name: "kartic",
+    message: `Greetings, I'm Kartic. My goal is to develop applications that enhance human experiences and productivity.`,
+    profilePic: ".../../public/assets/images/dp/4.jpg",
+  },
+  {
+    name: "sara",
+    message: `Hey there, I'm Sara. I'm dedicated to building user-centered applications that foster meaningful interactions.`,
+    profilePic: ".../../public/assets/images/dp/5.png",
+  },
+  {
+    name: "meg",
+    message: `Hi, I'm Meg. I'm passionate about creating intuitive applications that simplify complex tasks.`,
+    profilePic: ".../../public/assets/images/dp/6.jpg",
+  },
+  {
+    name: "piyush",
+    message: `Hello, I'm Piyush. I enjoy developing innovative applications that push the boundaries of technology.`,
+    profilePic: ".../../public/assets/images/dp/7.png",
+  },
+  {
+    name: "olivia",
+    message: `Greetings, I'm Olivia. My focus is on crafting applications that enrich people's lives through creativity and efficiency.`,
+    profilePic: ".../../public/assets/images/dp/8.jpg",
+  },
+];
+
 const Messages = () => {
   return (
     <div>
       <CommonHeader heading={"Messages"} />
-      {Array(12)
+      {Array(8)
         .fill("")
-        .map((index) => {
-          return <MessagePreview />;
+        .map((_, index) => {
+          return (
+            <MessagePreview
+              name={messages[index].name}
+              text={messages[index].message}
+              dp={messages[index].profilePic}
+            />
+          );
         })}
     </div>
   );
