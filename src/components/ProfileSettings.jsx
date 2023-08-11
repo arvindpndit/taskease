ce0401af4ece108825dee4e4bd5b09f4c7cf786c
arvindpndit <arvindpandit9808@gmail.com>
2023-08-11 22:15:55 +0530

    changed discs to icons

--- src/components/ProfileSettings.jsx
+++ src/components/ProfileSettings.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import {
+  IoCardOutline,
+  IoPersonOutline,
+  IoAppsOutline,
+  IoShieldCheckmarkOutline,
+  IoChatbubblesOutline,
+  IoDocumentOutline,
+  IoLanguageOutline,
+  IoAlertCircleOutline,
+  IoNotificationsCircleOutline,
+  IoCloudUploadOutline,
+} from "react-icons/io5";
 
 const ProfileSettings = () => {
   return (
@@ -7,40 +19,76 @@ const ProfileSettings = () => {
         {/* Payment Settings */}
         <div className="bg-gray-100 rounded-lg p-4">
           <h3 className="font-semibold text-lg mb-2">PAYMENT SETTINGS</h3>
-          <ul className="list-disc pl-6">
-            <li>Transaction history</li>
-            <li>Update payment</li>
+          <ul className="list-none pl-6">
+            <li className="flex items-center">
+              <IoCardOutline className="mr-2 text-gray-500" />
+              Transaction history
+            </li>
+            <li className="flex items-center">
+              <IoCloudUploadOutline className="mr-2 text-gray-500" />
+              Update payment
+            </li>
           </ul>
         </div>
 
         {/* Personal */}
         <div className="bg-gray-100 rounded-lg p-4">
           <h3 className="font-semibold text-lg mb-2">PERSONAL</h3>
-          <ul className="list-disc pl-6">
-            <li>Notification preferences</li>
-            <li>Skills</li>
+          <ul className="list-none pl-6">
+            <li className="flex items-center">
+              <IoNotificationsCircleOutline className="mr-2 text-gray-500" />
+              Notification preferences
+            </li>
+            <li className="flex items-center">
+              <IoPersonOutline className="mr-2 text-gray-500" />
+              Skills
+            </li>
           </ul>
         </div>
 
         {/* More */}
         <div className="bg-gray-100 rounded-lg p-4">
           <h3 className="font-semibold text-lg mb-2">MORE</h3>
-          <ul className="list-disc pl-6">
-            <li>Set up task alerts</li>
-            <li>Community guidelines</li>
-            <li>Language</li>
+          <ul className="list-none pl-6">
+            <li className="flex items-center">
+              <IoAppsOutline className="mr-2 text-gray-500" />
+              Set up task alerts
+            </li>
+            <li className="flex items-center">
+              <IoChatbubblesOutline className="mr-2 text-gray-500" />
+              Community guidelines
+            </li>
+            <li className="flex items-center">
+              <IoLanguageOutline className="mr-2 text-gray-500" />
+              Language
+            </li>
           </ul>
         </div>
 
         {/* Safety */}
         <div className="bg-gray-100 rounded-lg p-4 col-span-2">
           <h3 className="font-semibold text-lg mb-2">SAFETY</h3>
-          <ul className="list-disc pl-6">
-            <li>Terms and conditions</li>
-            <li>Contact us</li>
-            <li>Update number</li>
-            <li>Delete my account</li>
-            <li>Sign out</li>
+          <ul className="list-none pl-6">
+            <li className="flex items-center">
+              <IoShieldCheckmarkOutline className="mr-2 text-gray-500" />
+              Terms and conditions
+            </li>
+            <li className="flex items-center">
+              <IoAlertCircleOutline className="mr-2 text-gray-500" />
+              Contact us
+            </li>
+            <li className="flex items-center">
+              <IoDocumentOutline className="mr-2 text-gray-500" />
+              Update number
+            </li>
+            <li className="flex items-center">
+              <IoDocumentOutline className="mr-2 text-gray-500" />
+              Delete my account
+            </li>
+            <li className="flex items-center">
+              <IoPersonOutline className="mr-2 text-gray-500" />
+              Sign out
+            </li>
           </ul>
         </div>
       </div>
