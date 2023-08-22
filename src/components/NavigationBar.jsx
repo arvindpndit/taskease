6b5694571409d97439e1659f8d72e73b76f82ff4
arvindpndit <arvindpandit9808@gmail.com>
2023-08-22 21:47:15 +0530

    fixed: navbar click effect

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NavigationBar = () => {
   return (
     <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full md:w-4/5 mx-auto md:px-6 lg:px-8 bg-white  p-5">
-      <ul className="flex justify-between text-xl  font-semibold hover:cursor-pointer">
+      <div className="flex justify-between text-xl  font-semibold">
         <Link to="/">
           <FaHome />
         </Link>
@@ -21,7 +21,7 @@ const NavigationBar = () => {
         <Link to="/profile">
           <FaUser />
         </Link>
-      </ul>
+      </div>
     </div>
   );
 };
